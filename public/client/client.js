ee3f2cab2ab89dea3b39bb1fd01842c6b702b2aa
Tom91011 <tomnightingale@hotmail.com>
2022-04-08 11:22:18 +0100

    Add js function for hiding elements and adding textContent

--- public/client/client.js
+++ public/client/client.js
@@ -1,13 +1,24 @@
 const countryTemplate = document.querySelector(".country-template")
 const countriesContainer = document.querySelector(".countries")
 
-const cloneNode = (template, container) => {
+const unhideElement = (element, cssClass) => element.classList.remove(cssClass)
+const addTextContent = (element, text) => element.textContent = text
+
+// This function copies a template
+const cloneNode = (template, container, data) => {
     const clonedTemplate = template.cloneNode(true)
     container.appendChild(clonedTemplate)
+    newCountry = container.lastChild
+    const countryPopulation = newCountry.querySelector(".country__population")
+    const countryCapital = newCountry.querySelector(".country__capital")
+    addTextContent(countryPopulation, `Population: ${data}`)
+    addTextContent(countryCapital, `Capital: Lisbon`)
+    unhideElement(newCountry, "country_hidden")
 }
 
 let i = 0
 while (i < 10) {
-    cloneNode(countryTemplate, countriesContainer)
+    data = (i + 1) * 1000
+    cloneNode(countryTemplate, countriesContainer, data)
     i++
 }
